eb37f6632840c1ec81577f415755dce3457db1a0
Riviets <vladvladpobuta@gmal.com>
2025-04-09 14:24:59 +0300

    Added point light and ambiend light to the scene. Added gridhelper and user OrbitControls to move the scene. Randomly populated the scene with spheres(starts)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './styles.css'
 
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene()
 
@@ -15,9 +16,36 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setZ(30)
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 ) 
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: true } ) 
+const material = new THREE.MeshStandardMaterial( { color: 0xffff00 } ) 
 const torus = new THREE.Mesh( geometry, material )
 scene.add( torus )
+
+const pointLight = new THREE.PointLight(0xffffff, 40)
+pointLight.position.set(10,8,10)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(ambientLight)
+
+const gridHelper = new THREE.GridHelper(200,50)
+
+scene.add(pointLight, pointLightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.15)
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x,y,z] = Array(3).fill().map(()=>THREE.MathUtils.randFloatSpread(100))
+
+  star.position.set(x,y,z)
+  scene.add(star)
+}
+
+Array(300).fill().forEach(addStar)
 
 function animate(){
   requestAnimationFrame(animate)
@@ -25,7 +53,6 @@ function animate(){
   torus.rotation.x += 0.01
   torus.rotation.y += 0.005
   torus.rotation.z += 0.01
-
 
   renderer.render(scene, camera)
 }
